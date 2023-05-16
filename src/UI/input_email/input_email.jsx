813519cb4ab9d7958837d1e9cf0b1fc5f0abe9a1
Wetall Sch <wetall55555@gmail.com>
2023-05-16 18:05:53 +0300

    add input

---
+++ src/UI/input_email/input_email.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./input_email";
+export const input_email = () => {
+  return (
+    <div className="email">
+      _email:
+      <p>
+        <input className="input_email" type="text" />
+      </p>
+    </div>
+  );
+};
