3ae58462d200713bff321160046463608f3c7f7b
Viktor <midyukinv@mail.ru>
2023-05-16 16:09:58 +0300

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { TextHome } from '../components/TextHome/TextHome'
 import { Shake } from '../components/Shake/Shake'
+import { Link } from "react-router-dom";
 export const Home = () => {
   return (
-    <div className='HomeCSS'>
+    <div className="HomeCSS">
       <TextHome />
       <Shake />
+      <Link to="about_me" >about_me</Link>
     </div>
   );
 }
