05942d9a9013cc8bafd08b2da5940c7050399731
Vsevolod9419 <smolin_9419@mail.ru>
2023-05-19 12:48:39 +0300

    add rightPlash

---
+++ src/UI/personalInfoLeft/PersonalInfoLeft.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const PersonalInfoLeft = () => {
+	return (
+		<select name="" id="">
+			
+		</select>
+	)
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Plash } from '../components/Plash/Plash'
+import { PersonalInfo } from '../components/PersonalInfo/PersonalInfo'
+import about from './About.module.css'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div className={about.container2}>
+     <Plash/>
+      <PersonalInfo/>
+    </div>
   )
 }
