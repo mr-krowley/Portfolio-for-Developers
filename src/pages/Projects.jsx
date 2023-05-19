bd31c2070ad08c7311682f0bb2206f746868dfb6
SergeyLoder <9790809@mail.ru>
2023-05-19 12:13:17 +0300

    add

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import { ProjectsSelect } from '../components/ProjectsSelect/ProjectsSelect';
 
+
 export const Projects = () => {
   return (
     <>
