4beb1f4315d0880074e63a2bc528b0e87de42b56
Wetall Sch <wetall55555@gmail.com>
2023-05-22 17:38:14 +0300

    add cards

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-
+import { ContainerCards } from '../components/containerCards/ContainerCards';
 export const Projects = () => {
   return (
     <>
-      <div>Projects</div>
+     <ContainerCards/> 
       <Link to="/">Home</Link>
     </>
   );
