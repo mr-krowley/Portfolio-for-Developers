50884f155ae9a18e871dc35cb8dc6d5f0077fe54
Wetall Sch <wetall55555@gmail.com>
2023-05-23 17:25:18 +0300

    add header_project_card

---
+++ src/UI/headerInsideProject/Header_inside_project.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import s from './Header_inside_project.module'
+export const Header_inside_project = () => {
+  return <p className={s.Header_inside_project}>React; Vue</p>;
+}
