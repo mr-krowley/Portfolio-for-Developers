904a7d2b6e93ec715abd449a6013cdcf9c486d2d
Vsevolod9419 <smolin_9419@mail.ru>
2023-05-23 11:51:47 +0300

    add dbAccardion

---
+++ src/UI/AccordionSPI/AccordionSPI.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { SubSubPersInf } from '../subSubPersInf/SubSubPersInf';
+import s from './AccordionSPI.module.css'
+
+
+export const AccordionSPI = ({src,  treug, title, active3, setActive3}) => {
+  console.log(title);
+	return (
+		<div className={s.accordian_box}>
+        <div className={s.acordionHeiding}>
+          <div>
+
+          </div>
+          <div
+            className={s.containerContacts}
+            onClick={() => {
+              active3 === title ? setActive3("") : setActive3(title);
+            }}
+          >
+             <img
+              src={treug}
+              alt="close"
+              className={
+                active3 === title
+                  ? s.accordion_heading_active
+                  : s.accordion_heading_not_active
+              } 
+            />
+            <img src ={src} alt="" />
+            <p>{title}</p>
+          </div>
+        </div>
+
+        <div className={active3 === title ? s.show : s.accordionContent}>
+          <div className="container">
+						<SubSubPersInf >high-school</SubSubPersInf>
+						<SubSubPersInf >universiti</SubSubPersInf>
+          </div>
+        </div>
+      </div>
+	)
+}
--- src/UI/subPersInf/SubPersInf.jsx
+++ src/UI/subPersInf/SubPersInf.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react'
-import vector2 from './aseds/Vector2.svg'
-import st from './SubPersInfo.module.css'
+import React, { useState } from "react";
+import vector2 from "./aseds/Vector2.svg";
+import st from "./SubPersInfo.module.css";
+import { AccordionSPI } from "../AccordionSPI/AccordionSPI";
 
-export const SubPersInf = ({src2, children}) => {
+export const SubPersInf = ({ src2, children }) => {
+  const [active3, setActive3] = useState("");
 
-	const [active3, setActive3] = useState('')
-
-	return ( 
-	<div className={st.style}>
-		<img src={vector2} alt="close" className={st.vector2}/>
-		<img src={src2} alt="" className={st.src2}/>
-		<p className={st.mail}>
-			{children}
-		</p>
+  return (<>
+    <div className={st.style}>
+      {/* <img src={vector2} alt="close" className={st.vector2} /> */}
+      {/* <img src={src2} alt="" className={st.src2} /> */}
+      {/* <p className={st.mail}>{children}</p> */}
+    </div>
+	<div>
+	<AccordionSPI src = {src2} treug = {vector2} title = {children} active3={active3} setActive3={setActive3}> </AccordionSPI>
 	</div>
-	)
-}
+		</>
+		  );
+};
