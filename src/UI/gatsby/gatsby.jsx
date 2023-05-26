7b5744ee138fa2f82f30a4cb283e37ce417ea69d
Vsevolod9419 <smolin_9419@mail.ru>
2023-05-26 19:17:33 +0300

    fixx

--- src/UI/gatsby/gatsby.jsx
+++ src/UI/gatsby/gatsby.jsx
@@ -4,7 +4,7 @@ import s from "../vue/Vue.module.css";
 
 export const Gatsby = () => {
   return (
-    <>
+    <> 
       <p className={s.wrapper}>
         <input className={s.input} type="checkbox" />
         <img className={s.img} src={GatsbyIcon} alt="gatsby" />
