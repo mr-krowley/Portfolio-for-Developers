0690117372e86528178225b6708276f8ddec3cc3
Wetall Sch <wetall55555@gmail.com>
2023-05-26 18:01:17 +0300

    add cards

--- src/UI/card_project/Card_project.js
+++ src/UI/card_project/Card_project.js
@@ -24,9 +24,63 @@ export let data = [
     //card_header лучше не изменять)
     card_header_txt: " //  _nobody needs Denis Novik",
     txt: "Bored with everyone Denis Novik with a crooked layout.",
-    img: "/images/etherium_img.png",
+    img: "./images/etherium_img.png",
     icon: "./images/icon/Icon_etherium.svg",
     id: "three",
     link: "https://github.com/wetall-sch/Novik",
+  },
+  {
+    card_header: "Project 4",
+    //card_header лучше не изменять)
+    card_header_txt: " //  _projects using HTML murkup",
+    txt: "Bored with everyone Denis Novik with a crooked layout.",
+    img: "./images/html_img.jpg",
+    icon: "./images/icon/html_icon.png",
+    id: "four",
+  },
+  {
+    card_header: "Project 5",
+    //card_header лучше не изменять)
+    card_header_txt: " //  _projects using CSS styling",
+    txt: "Bored with everyone Denis Novik with a crooked layout.",
+    img: "./images/css_image.png",
+    icon: "./images/icon/css_icon.png",
+    id: "five",
+  },
+  {
+    card_header: "Project 6",
+    //card_header лучше не изменять)
+    card_header_txt: " //  _projects with additional use of vue",
+    txt: "Bored with everyone Denis Novik with a crooked layout.",
+    img: "./images/vue_img.jpg",
+    icon: "./images/icon/Icon_etherium.svg",
+    id: "six",
+  },
+  {
+    card_header: "Project 7",
+    //card_header лучше не изменять)
+    card_header_txt: " //  _projects using the angular framework",
+    txt: "Bored with everyone Denis Novik with a crooked layout.",
+    img: "./images/angular_img.png",
+    icon: "./images/icon/html_icon.png",
+    id: "seven",
+  },
+  {
+    card_header: "Project 8",
+    //card_header лучше не изменять)
+    card_header_txt: " //  _projects using the react library",
+    txt: "Bored with everyone Denis Novik with a crooked layout.",
+    img: "./images/react_img.jpg",
+    icon: "./images/icon/Icon_react.svg",
+    id: "eight",
+  },
+  {
+    card_header: "Project 9",
+    //card_header лучше не изменять)
+    card_header_txt: " //  _another complex grandiose project",
+    txt: "Bored with everyone Denis Novik with a crooked layout.",
+    img: "./images/mdn_img.png",
+    icon: "./images/icon/mdn_logo.png",
+    id: "nine",
   },
 ];
